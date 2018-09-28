a362644fcd656a595226d3cb93155446c3b395e6
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-09-29 00:06:42 +0200

    moving on

--- app/src/main/java/com/example/adambenyahia/pizzaapp/MainActivity.java
+++ app/src/main/java/com/example/adambenyahia/pizzaapp/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.order) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.view_menu) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.todays_offers) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.extras) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.contact_us) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.rate_app) {
 
         }
 
